b393779e8f20188b5da5f48012ef44166c25f623
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-14 11:29:20 +0200

    Update 04-test.js

--- src/js/04-test.js
+++ src/js/04-test.js
@@ -1,3 +1,4 @@
+/*
 function processCall(recipient) {
   // Імітуємо доступність абонента випадковим числом
   const isRecipientAvailable = Math.random() > 0.5;
@@ -12,3 +13,34 @@ function processCall(recipient) {
 }
 
 processCall('Mango');
+*/
+
+function processCall(recipient, onAvailable, onNotAvailable) {
+  // Імітуємо доступність абонента випадковим числом
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    onNotAvailable(recipient);
+    return;
+  }
+
+  onAvailable(recipient);
+}
+
+function takeCall(name) {
+  console.log(`З'єднуємо з ${name}, чекайте...`);
+  // Логіка прийняття дзвінка
+}
+
+function activateAnsweringMachine(name) {
+  console.log(`Абонент ${name} недоступний, залиште повідомлення.`);
+  // Логіка активації автовідповідача
+}
+
+function leaveHoloMessage(name) {
+  console.log(`Абонент ${name} недоступний, записуємо голограму.`);
+  // Логіка запису голограми
+}
+
+processCall('Mango', takeCall, activateAnsweringMachine);
+processCall('Poly', takeCall, leaveHoloMessage);
