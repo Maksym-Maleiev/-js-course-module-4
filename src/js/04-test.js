d9da9f07d62c81d71d61ffae0a59aef7e43b7a1d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-14 11:25:23 +0200

    Update 04-test.js

---
+++ src/js/04-test.js
@@ -0,0 +1,14 @@
+function processCall(recipient) {
+  // Імітуємо доступність абонента випадковим числом
+  const isRecipientAvailable = Math.random() > 0.5;
+
+  if (!isRecipientAvailable) {
+    console.log(`Абонент ${recipient} недоступний, залиште повідомлення.`);
+    // Логіка активації автовідповідача
+  } else {
+    console.log(`З'єднуємо з ${recipient}, чекайте...`);
+    // Логіка прийняття дзвінка
+  }
+}
+
+processCall('Mango');
