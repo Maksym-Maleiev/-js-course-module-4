7a1a7d786185f86be826e343bf52b3e09ee241ef
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-14 10:40:33 +0200

    Update 03-test.js

--- src/js/03-test.js
+++ src/js/03-test.js
@@ -14,3 +14,15 @@ registerGuest('Poly', function notify(name) {
   console.log(` Шановний ${name}, ваш номер буде готовий через 30 хвилин.`);
 });
 */
+
+function makePizza(pizzaName, callback) {
+  console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+  callback(pizzaName);
+}
+
+makePizza('Royal Grand', function deliverPizza(pizzaName) {
+  console.log(`Delivering pizza ${pizzaName}.`);
+});
+// Change code below this line
+
+makePizza('Ultracheese');
