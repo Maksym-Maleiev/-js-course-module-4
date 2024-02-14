9f2a74dff208d1a4aa8ec3bee6b48b036c5d827a
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-14 10:28:02 +0200

    Update 02-test.js

--- src/js/02-test.js
+++ src/js/02-test.js
@@ -10,3 +10,16 @@ function registerGuest(name, callback) {
 }
 registerGuest('Mango', greet);
 */
+
+function deliverPizza(pizzaName) {
+  return `Delivering ${pizzaName} pizza.`;
+}
+
+function makePizza(pizzaName) {
+  return `Pizza ${pizzaName} is being prepared, please wait...`;
+}
+
+// Chande code below this line
+function makeMessage(pizzaName) {
+  return;
+}
