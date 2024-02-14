1abaa7c878ef3105fdd5cf0b8f86dd560bc6f5fc
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-14 10:27:37 +0200

    Update 02-test.js

---
+++ src/js/02-test.js
@@ -0,0 +1,10 @@
+// Колбек-функція
+function greet(name) {
+  console.log(`Ласкаво просимо ${name}.`);
+}
+// Функція вищого порядку
+function registerGuest(name, callback) {
+  console.log(`Реєструємо гостя ${name}.`);
+  callback(name);
+}
+registerGuest('Mango', greet);
