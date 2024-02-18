d14425e4f5cd045362602abd4a18639890344b0c
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 11:34:36 +0200

    Update 05-test.js

--- src/js/05-test.js
+++ src/js/05-test.js
@@ -23,3 +23,7 @@ function calculateTotalPrice(orderedItems) {
   // Change code above this line
   return totalPrice;
 }
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
