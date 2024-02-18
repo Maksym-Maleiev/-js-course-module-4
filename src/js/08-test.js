8350f47e6f3bfe7534228098667abe681110c176
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 21:44:40 +0200

    Update 08-test.js

---
+++ src/js/08-test.js
@@ -0,0 +1,28 @@
+/*
+function getFirstPosition(values, value) {
+  for (let i = 0; i < values.length; i += 1) {
+    const element = values[i];
+    if (element === value) {
+      return i;
+    }
+  }
+
+  return -1;
+}
+*/
+
+function getFirstPosition(values, value) {
+  for (const el of values) {
+    const index = values.indexOf(el);
+
+    if (el === value) {
+      return index;
+    }
+  }
+
+  return -1;
+}
+
+console.log(getFirstPosition(['one', 'two', 'three'], 'three'));
+
+console.log(getFirstPosition([1, 2, 3, 4], 99));
