211fab40c9199053d90d21efde785a783bb0b040
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 11:40:14 +0200

    Update 06-test.js

--- src/js/06-test.js
+++ src/js/06-test.js
@@ -2,11 +2,17 @@ function filterArray(numbers, value) {
   const filteredNumbers = [];
   // Change code below this line
 
-  for (let i = 0; i < numbers.length; i += 1) {
-    if (numbers[i] > value) {
-      filteredNumbers.push(numbers[i]);
+  // for (let i = 0; i < numbers.length; i += 1) {
+  //   if (numbers[i] > value) {
+  //     filteredNumbers.push(numbers[i]);
+  //   }
+  // }
+
+  numbers.foreach(function (number) {
+    if (number > value) {
+      filteredNumbers.push(number);
     }
-  }
+  });
 
   // Change code above this line
   return filteredNumbers;
