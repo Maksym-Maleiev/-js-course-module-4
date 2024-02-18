dbab50bfe878379122290125d709c5a673957b70
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 11:37:06 +0200

    Update 05-test.js

--- src/js/05-test.js
+++ src/js/05-test.js
@@ -16,9 +16,9 @@ function calculateTotalPrice(orderedItems) {
   let totalPrice = 0;
   // Change code below this line
 
-  for (let i = 0; i < orderedItems.length; i += 1) {
-    totalPrice += orderedItems[i];
-  }
+  orderedItems.forEach(function (number) {
+    totalPrice += number;
+  });
 
   // Change code above this line
   return totalPrice;
