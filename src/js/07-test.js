6e275a201b74b71ed01aaac59ebd900ef24d5d7f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 11:47:53 +0200

    Update 07-test.js

--- src/js/07-test.js
+++ src/js/07-test.js
@@ -14,3 +14,5 @@ function getCommonElements(firstArray, secondArray) {
 
 console.log(getCommonElements([1, 2, 3], [2, 4]));
 console.log(getCommonElements([1, 2, 3], [2, 1, 17, 19]));
+console.log(getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]));
+console.log(getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]));
