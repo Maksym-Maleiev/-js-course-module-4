342e12c686029e3e41f7f49ecf6a5bba4450e8cd
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 11:33:47 +0200

    Update 05-test.js

--- src/js/05-test.js
+++ src/js/05-test.js
@@ -11,3 +11,15 @@ numbers.forEach(function (number, index) {
   console.log(`Індекс ${index}, значення ${number}`);
 });
 */
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+
+  for (let i = 0; i < orderedItems.length; i += 1) {
+    totalPrice += orderedItems[i];
+  }
+
+  // Change code above this line
+  return totalPrice;
+}
