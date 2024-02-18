0b3aa8bc5565b7ef5b97eaae9fb65e5c5d368d83
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 11:47:21 +0200

    Update 07-test.js

--- src/js/07-test.js
+++ src/js/07-test.js
@@ -2,12 +2,14 @@ function getCommonElements(firstArray, secondArray) {
   const commonElements = [];
   // Change code below this line
 
-  for (let i = 0; i < firstArray.length; i += 1) {
-    if (secondArray.includes(firstArray[i])) {
-      commonElements.push(firstArray[i]);
-    }
-  }
+  // for (let i = 0; i < firstArray.length; i += 1) {
+  //   if (secondArray.includes(firstArray[i])) {
+  //     commonElements.push(firstArray[i]);
+  //   }
+  // }
 
   return commonElements;
   // Change code above this line
 }
+
+console.log(getCommonElements([1, 2, 3], [2, 4]));
