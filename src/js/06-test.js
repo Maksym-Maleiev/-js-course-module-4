2749a9198d10d5c109d625d609c6366fbd214176
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-18 11:41:24 +0200

    Update 06-test.js

--- src/js/06-test.js
+++ src/js/06-test.js
@@ -15,4 +15,4 @@ function filterArray(numbers, value) {
 console.log(filterArray([1, 2, 3, 4, 5], 3));
 console.log(filterArray([1, 2, 3, 4, 5], 4));
 console.log(filterArray([1, 2, 3, 4, 5], 5));
-console.log();
+console.log(filterArray([12, 24, 8, 41, 76], 38));
