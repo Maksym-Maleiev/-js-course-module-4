94f3cc4244c80ac9ca712a7d6ddc5b34045e8820
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-19 10:38:44 +0200

    Update 10-test.js

--- src/js/10-test.js
+++ src/js/10-test.js
@@ -46,13 +46,17 @@ numbers.forEach(logMessage);
 */
 
 // Change code below this line
-function calculateTotalPrice(orderedItems) {
+const calculateTotalPrice = orderedItems => {
   let totalPrice = 0;
 
-  orderedItems.forEach(function (item) {
+  orderedItems.forEach(item => {
     totalPrice += item;
   });
 
   return totalPrice;
-}
+};
 // Change code above this line
+
+console.log(calculateTotalPrice([12, 85, 37, 4]));
+console.log(calculateTotalPrice([164, 48, 291]));
+console.log(calculateTotalPrice([412, 371, 94, 63, 176]));
