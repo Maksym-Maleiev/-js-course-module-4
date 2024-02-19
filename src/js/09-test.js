2bf6be3d29448a320a27b89b0ea96ee49e5c3bc6
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-19 09:48:35 +0200

    Update 09-test.js

--- src/js/09-test.js
+++ src/js/09-test.js
@@ -3,3 +3,7 @@ const calculateTotalPrice = (quantity, pricePerItem) => {
   return quantity * pricePerItem;
 };
 // Change code above this line
+
+console.log(calculateTotalPrice(5, 100));
+console.log(calculateTotalPrice(8, 60));
+console.log(calculateTotalPrice(3, 400));
