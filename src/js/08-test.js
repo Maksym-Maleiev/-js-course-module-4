6b31f36f1f2abecba0f2aafb9c6766a864d12976
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-19 09:42:54 +0200

    Update 08-test.js

--- src/js/08-test.js
+++ src/js/08-test.js
@@ -28,3 +28,14 @@ console.log(getFirstPosition(['one', 'two', 'three'], 'three'));
 
 console.log(getFirstPosition([1, 2, 3, 4], 99));
 */
+
+// Change code below this line
+
+const calculateTotalPrice = (quantity, pricePerItem) => {
+  // Change code above this line
+  return quantity * pricePerItem;
+};
+
+console.log(calculateTotalPrice(5, 100));
+console.log(calculateTotalPrice(8, 60));
+console.log(calculateTotalPrice(3, 400));
