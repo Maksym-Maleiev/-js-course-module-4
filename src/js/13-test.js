228e6e7a3167ac64cadc576b4aa85c9cb1288d12
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-23 15:21:46 +0200

    Update 13-test.js

--- src/js/13-test.js
+++ src/js/13-test.js
@@ -1,3 +1,5 @@
+//! dirty function
+/*
 const dirtyMultiply = (array, value) => {
   for (let i = 0; i < array.length; i += 1) {
     array[i] = array[i] * value;
@@ -7,3 +9,4 @@ const numbers = [1, 2, 3, 4, 5];
 dirtyMultiply(numbers, 2);
 // Відбулася мутація вихідних даних – масиву numbers console.log(numbers);
 // [2, 4, 6, 8, 10]
+*/
