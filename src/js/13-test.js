80ebed39fa5f7dab55c0e47773a377f49698ecea
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-23 15:31:47 +0200

    Update 13-test.js

--- src/js/13-test.js
+++ src/js/13-test.js
@@ -44,7 +44,7 @@ function changeEven(numbers, value) {
 }
 // Change code above this line
 
-console.log(changeEven([1, 2, 3, 4, 5], 10));
-console.log(changeEven([2, 8, 3, 7, 4, 6], 10));
-console.log(changeEven([17, 24, 68, 31, 42], 100));
-console.log(changeEven([44, 13, 81, 92, 36, 54], 100));
+console.log(changeEven([1, 2, 3, 4, 5], 10)); // [1, 12, 3, 14, 5]
+console.log(changeEven([2, 8, 3, 7, 4, 6], 10)); // [12, 18, 3, 7, 14, 16]
+console.log(changeEven([17, 24, 68, 31, 42], 100)); // [17, 124, 168, 31, 142]
+console.log(changeEven([44, 13, 81, 92, 36, 54], 100)); // [144, 13, 81, 192, 136, 154]
