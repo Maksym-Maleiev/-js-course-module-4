d23c95f68d1076b847a03f07ef21e509576f879e
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-24 08:40:13 +0200

    Update 14-test.js

--- src/js/14-test.js
+++ src/js/14-test.js
@@ -12,3 +12,7 @@ console.log(planetsInLowerCase);
 console.log(planets);
 // ["Earth", "Mars", "Venus", "Jupiter"]
 */
+
+const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
+// Change code below this line
+const planetsLengths = planets;
