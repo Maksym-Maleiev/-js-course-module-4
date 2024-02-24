68693ac261d4b53f8ef2fa13844073d98e1a177d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-24 08:36:54 +0200

    Update 14-test.js

--- src/js/14-test.js
+++ src/js/14-test.js
@@ -1,3 +1,11 @@
 const planets = ['Earth', 'Mars', 'Venus', 'Jupiter'];
 const planetsInUpperCase = planets.map(planet => planet.toUpperCase());
-console.log(planetsInUpperCase); // ["EARTH", "MARS", "VENUS", "JUPITER"] const planetsInLowerCase = planets.map(planet => planet.toLowerCase()); console.log(planetsInLowerCase); // ["earth", "mars", "venus", "jupiter"] // Оригінальний масив не змінився console.log(planets); // ["Earth", "Mars", "Venus", "Jupiter"]
+
+console.log(planetsInUpperCase);
+// ["EARTH", "MARS", "VENUS", "JUPITER"]
+
+const planetsInLowerCase = planets.map(planet => planet.toLowerCase());
+console.log(planetsInLowerCase);
+// ["earth", "mars", "venus", "jupiter"]
+// Оригінальний масив не змінився console.log(planets);
+// ["Earth", "Mars", "Venus", "Jupiter"]
