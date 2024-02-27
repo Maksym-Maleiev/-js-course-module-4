095c5c75dbacdf4f31877dd79e7f1888d837d471
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-27 13:40:51 +0200

    Update 15-test.js

--- src/js/15-test.js
+++ src/js/15-test.js
@@ -31,3 +31,26 @@ const students = [
 const names = students.map(student => student.name);
 console.log(names);
 // ["Mango", "Poly", "Ajax", "Kiwi", "Houston"]
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+];
+// Change code below this line
+
+const titles = books;
