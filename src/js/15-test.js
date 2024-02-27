6450ce447893e65d876f0a1ccc1cae8062ce5651
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-27 13:39:07 +0200

    Update 15-test.js

--- src/js/15-test.js
+++ src/js/15-test.js
@@ -19,3 +19,15 @@ console.log(createArray(1));
 console.log(createArray(3));
 console.log(createArray(0));
 */
+
+const students = [
+  { name: 'Mango', score: 83 },
+  { name: 'Poly', score: 59 },
+  { name: 'Ajax', score: 37 },
+  { name: 'Kiwi', score: 94 },
+  { name: 'Houston', score: 64 },
+];
+
+const names = students.map(student => student.name);
+console.log(names);
+// ["Mango", "Poly", "Ajax", "Kiwi", "Houston"]
