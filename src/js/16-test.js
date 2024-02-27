4ab3609fd091cbc326674b788b842adfff458252
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-27 13:45:42 +0200

    Update 16-test.js

--- src/js/16-test.js
+++ src/js/16-test.js
@@ -9,3 +9,24 @@ console.log(students.map(student => student.courses));
 
 console.log(students.flatMap(student => student.courses));
 // ["mathematics", "physics", "science", "mathematics", "physics", "biology"];
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    genres: ['adventure', 'history'],
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    genres: ['fiction'],
+  },
+  {
+    title: 'Redder Than Blood',
+    author: 'Tanith Lee',
+    genres: ['horror', 'mysticism'],
+  },
+];
+// Change code below this line
+
+const genres = books;
