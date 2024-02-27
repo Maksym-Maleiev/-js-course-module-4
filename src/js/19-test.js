05d36ee1d79f65f00cef544347f7608fd2a7ee80
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-27 14:02:48 +0200

    Update 19-test.js

---
+++ src/js/19-test.js
@@ -0,0 +1,15 @@
+const values = [51, -3, 27, 21, -68, 42, -37];
+
+const positiveValues = values.filter(value => value >= 0);
+console.log(positiveValues);
+// [51, 27, 21, 42]
+
+const negativeValues = values.filter(value => value < 0);
+console.log(negativeValues); // [-3, -68, -37]
+
+const bigValues = values.filter(value => value > 1000);
+console.log(bigValues);
+//[]
+
+// Оригінальний масив не змінився
+console.log(values); // [51, -3, 27, 21, -68, 42, -37]
