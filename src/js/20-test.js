60245a9ae8305d4056f624d473f4d68c8c44f93e
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-27 15:21:10 +0200

    Update 20-test.js

--- src/js/20-test.js
+++ src/js/20-test.js
@@ -14,3 +14,24 @@ const uniqueCourses = allCourses.filter(
 
 console.log(uniqueCourses);
 */
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    genres: ['adventure', 'history'],
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    genres: ['fiction', 'mysticism'],
+  },
+  {
+    title: 'Redder Than Blood',
+    author: 'Tanith Lee',
+    genres: ['horror', 'mysticism', 'adventure'],
+  },
+];
+// Change code below this line
+const allGenres = books;
+const uniqueGenres = allGenres;
