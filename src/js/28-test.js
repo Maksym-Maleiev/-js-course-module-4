0fe004a59f0811c46ee2540170ebd219570367e1
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-28 15:46:34 +0200

    Update 28-test.js

--- src/js/28-test.js
+++ src/js/28-test.js
@@ -18,3 +18,28 @@ console.log(colorPickerOptions.find(option => option.label === 'pink'));
 
 console.log(colorPickerOptions.find(option => option.label === 'white'));
 // undefined
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+];
+const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+const AUTHOR = 'Robert Sheckley';
+// Change code below this line
+
+const bookWithTitle = books;
+const bookByAuthor = books;
