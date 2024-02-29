6fb4512ff3e0cf71b568fc53014d67c73bb0233d
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-29 13:15:43 +0200

    Update 34-test.js

--- src/js/34-test.js
+++ src/js/34-test.js
@@ -7,3 +7,21 @@ const total = [2, 7, 3, 14, 6].reduce((previousValue, number) => {
 }, 0);
 
 console.log(total); // 32
+
+/*
+ * Exercise
+ */
+
+const players = {
+  mango: 1270,
+  poly: 468,
+  ajax: 710,
+  kiwi: 244,
+};
+const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// Change code below this line
+
+const totalPlayTime = playtimes;
+
+// Change code above this line
+const averagePlayTime = totalPlayTime / playtimes.length;
