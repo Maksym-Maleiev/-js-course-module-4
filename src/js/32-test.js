cb170a48b5e3c6139733b5fd2e197bb5413df785
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-29 13:07:13 +0200

    Update 32-test.js

--- src/js/32-test.js
+++ src/js/32-test.js
@@ -17,3 +17,21 @@ console.log([1, 2, 3, 4, 5].some(value => value < 0));
 // Чи є хоча б один елемент менше нуля? - Так
 console.log([1, 2, 3, -10, 4, 5].some(value => value < 0));
 // true
+
+/*
+ * Exercise
+ */
+
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change below this line
+
+const anyElementInFirstIsEven = firstArray;
+const anyElementInFirstIsOdd = firstArray;
+
+const anyElementInSecondIsEven = secondArray;
+const anyElementInSecondIsOdd = secondArray;
+
+const anyElementInThirdIsEven = thirdArray;
+const anyElementInThirdIsOdd = thirdArray;
