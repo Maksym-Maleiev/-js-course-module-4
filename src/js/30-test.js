3739ee4404cfe04afcea5e827b8e6ec0537806c5
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-29 12:54:29 +0200

    Update 30-test.js

--- src/js/30-test.js
+++ src/js/30-test.js
@@ -13,3 +13,17 @@ console.log([1, 2, 3, -10, 4, 5].every(value => value >= 0));
 /*
  * Exercise
  */
+
+const firstArray = [26, 94, 36, 18];
+const secondArray = [17, 61, 23];
+const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// Change code below this line
+
+const eachElementInFirstIsEven = firstArray;
+const eachElementInFirstIsOdd = firstArray;
+
+const eachElementInSecondIsEven = secondArray;
+const eachElementInSecondIsOdd = secondArray;
+
+const eachElementInThirdIsEven = thirdArray;
+const eachElementInThirdIsOdd = thirdArray;
