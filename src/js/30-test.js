efebc5e81d9082dba8acbe9f2263aa363ac0219b
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-02-29 12:55:30 +0200

    Update 30-test.js

--- src/js/30-test.js
+++ src/js/30-test.js
@@ -19,7 +19,7 @@ const secondArray = [17, 61, 23];
 const thirdArray = [17, 26, 94, 61, 36, 23, 18];
 // Change code below this line
 
-const eachElementInFirstIsEven = firstArray;
+const eachElementInFirstIsEven = firstArray.every(number => number % 2 === 0);
 const eachElementInFirstIsOdd = firstArray;
 
 const eachElementInSecondIsEven = secondArray;
