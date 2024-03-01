c359a004c0829e6a676ce1159a0a8f1e40e10da8
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:41:00 +0200

    Update 39-test.js

--- src/js/39-test.js
+++ src/js/39-test.js
@@ -11,3 +11,14 @@ const score = [61, 19, 74, 35, 92, 56];
 const descendingScores = [...score].sort((a, b) => b - a);
 console.log(descendingScores);
 // [92, 74, 61, 56, 35, 19]
+
+/*
+ * Exercise
+ */
+
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+// Change code below this line
+
+const ascendingReleaseDates = releaseDates;
+
+const descendingReleaseDates = releaseDates;
