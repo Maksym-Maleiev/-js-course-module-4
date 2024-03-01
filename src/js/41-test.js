60e4b500dc383cac2bc70a5f0f46aaaf7aa52d1f
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:52:19 +0200

    Update 41-test.js

--- src/js/41-test.js
+++ src/js/41-test.js
@@ -1,3 +1,24 @@
 /*
  * Sorting objects
  */
+
+const students = [
+  { name: 'Mango', score: 83 },
+  { name: 'Poly', score: 59 },
+  { name: 'Ajax', score: 37 },
+  { name: 'Kiwi', score: 94 },
+];
+
+const inAscendingScoreOrder = [...students].sort(
+  (firstStudent, secondStudent) => firstStudent.score - secondStudent.score
+);
+const inDescendingScoreOrder = [...students].sort(
+  (firstStudent, secondStudent) => secondStudent.score - firstStudent.score
+);
+const inAlphabeticalOrder = [...students].sort((firstStudent, secondStudent) =>
+  firstStudent.name.localeCompare(secondStudent.name)
+);
+
+console.log(inAscendingScoreOrder);
+console.log(inDescendingScoreOrder);
+console.log(inAlphabeticalOrder);
