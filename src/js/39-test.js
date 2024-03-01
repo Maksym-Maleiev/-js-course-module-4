6152ceb41350d6c442c7081c763792c1557e33bc
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:39:56 +0200

    Update 39-test.js

--- src/js/39-test.js
+++ src/js/39-test.js
@@ -1,3 +1,8 @@
 /*
  * Custom number sorting order
  */
+
+const scores = [61, 19, 74, 35, 92, 56];
+const ascendingScores = [...scores].sort((a, b) => a - b);
+console.log(ascendingScores);
+// [19, 35, 56, 61, 74, 92]
