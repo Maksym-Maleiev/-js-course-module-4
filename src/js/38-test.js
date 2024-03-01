a64fe305adcdf7e136171f98569672edee7e5800
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:05:36 +0200

    Update 38-test.js

--- src/js/38-test.js
+++ src/js/38-test.js
@@ -1,3 +1,30 @@
 /*
  * Method 'sort()'
  */
+
+const scores = [61, 19, 74, 35, 92, 56];
+scores.sort();
+console.log(scores);
+// [19, 35, 56, 61, 74, 92]
+
+const score = [27, 2, 41, 4, 7, 3, 75];
+score.sort();
+console.log(score);
+// [2, 27, 3, 4, 41, 7, 75]
+
+const students = ['Jacob', 'Artemis', 'Solomon', 'Adrian', 'Kai', 'Ganymede'];
+students.sort();
+console.log(students);
+// [ "Adrian", "Artemis", "Ganymede", "Jacob", "Kai", "Solomon" ]
+
+const letters = ['b', 'B', 'a', 'A', 'c', 'C'];
+letters.sort();
+console.log(letters);
+// ["A", "B", "C", "a", "b", "c"]
+
+const scor = [61, 19, 74, 35, 92, 56];
+const ascendingScor = [...scor].sort();
+console.log(scor);
+// [61, 19, 74, 35, 92, 56]
+console.log(ascendingScor);
+// [19, 35, 56, 61, 74, 92]
