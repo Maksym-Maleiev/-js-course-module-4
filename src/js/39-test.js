869ffed80214dee5938f7e96b4a6662226345eb2
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:43:21 +0200

    Update 39-test.js

--- src/js/39-test.js
+++ src/js/39-test.js
@@ -22,3 +22,6 @@ const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
 const ascendingReleaseDates = [...releaseDates].sort((a, b) => a - b);
 
 const descendingReleaseDates = [...releaseDates].sort((a, b) => b - a);
+
+console.log(ascendingReleaseDates);
+console.log(descendingReleaseDates);
