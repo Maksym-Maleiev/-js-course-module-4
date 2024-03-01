2bca490356f5801f2491acc1beafada1e589b6a0
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:06:22 +0200

    Update 38-test.js

--- src/js/38-test.js
+++ src/js/38-test.js
@@ -32,3 +32,16 @@ console.log(ascendingScor);
 /*
  * Exercise
  */
+
+const releaseDates = [2016, 1967, 2008, 1984, 1973, 2012, 1997];
+const authors = [
+  'Tanith Lee',
+  'Bernard Cornwell',
+  'Robert Sheckley',
+  'Fyodor Dostoevsky',
+];
+// Change code below this line
+
+const ascendingReleaseDates = releaseDates;
+
+const alphabeticalAuthors = authors;
