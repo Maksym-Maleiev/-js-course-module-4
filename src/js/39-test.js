2dd558959337dfa249f20dc2f080f69393a434f3
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:40:13 +0200

    Update 39-test.js

--- src/js/39-test.js
+++ src/js/39-test.js
@@ -6,3 +6,8 @@ const scores = [61, 19, 74, 35, 92, 56];
 const ascendingScores = [...scores].sort((a, b) => a - b);
 console.log(ascendingScores);
 // [19, 35, 56, 61, 74, 92]
+
+const score = [61, 19, 74, 35, 92, 56];
+const descendingScores = [...score].sort((a, b) => b - a);
+console.log(descendingScores);
+// [92, 74, 61, 56, 35, 19]
