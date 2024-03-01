c419bbdd4175cabcdd43c4dcc8376c2d671b5ef0
Maksym-Maleiev <maximmaleev93@gmail.com>
2024-03-01 13:53:47 +0200

    Update 41-test.js

--- src/js/41-test.js
+++ src/js/41-test.js
@@ -24,3 +24,36 @@ const inAlphabeticalOrder = [...students].sort((firstStudent, secondStudent) =>
 console.log(inAscendingScoreOrder);
 console.log(inDescendingScoreOrder);
 console.log(inAlphabeticalOrder);
+
+/*
+ * Exercise
+ */
+
+const books = [
+  {
+    title: 'The Last Kingdom',
+    author: 'Bernard Cornwell',
+    rating: 8.38,
+  },
+  {
+    title: 'Beside Still Waters',
+    author: 'Robert Sheckley',
+    rating: 8.51,
+  },
+  {
+    title: 'The Dream of a Ridiculous Man',
+    author: 'Fyodor Dostoevsky',
+    rating: 7.75,
+  },
+  { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+  { title: 'Enemy of God', author: 'Bernard Cornwell', rating: 8.67 },
+];
+// Change code below this line
+
+const sortedByAuthorName = books;
+
+const sortedByReversedAuthorName = books;
+
+const sortedByAscendingRating = books;
+
+const sortedByDescentingRating = books;
